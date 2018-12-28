2d4c13863b7bfd25744144622c14ba429d722541
lihaoyu910774624 <46072035+lihaoyu910774624@users.noreply.github.com>
2018-12-28 10:21:56 +0800

    分支合并

--- SSM/src/main/java/com/yuanjun/front/wxjs/WxjsConfig.java
+++ SSM/src/main/java/com/yuanjun/front/wxjs/WxjsConfig.java
@@ -2,8 +2,8 @@ package com.yuanjun.front.wxjs;
 
 public class WxjsConfig {
 	
-	 public static final String APPID ="wxe7fb29f39f1f47b2";
-	 public static final String APPSECRET = "e3ec5b39f411d588b8b650757672861b";	
+	 public static final String APPID ="wxe82d18c4c4bf00da";
+	 public static final String APPSECRET = "8748ca640b37819fc80145934fa6b3f1	";	
 	 public static final String   TOKENID   = "gkgj_token";
 	 public static final String   JSTICKET   = "gkgj_jstic";
 
